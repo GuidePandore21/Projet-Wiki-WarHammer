3522d71c4ee5028816aac0553ce90607ab0a89fc
GuidePandore21 <tlyautey1@gmail.com>
2022-03-08 11:36:39 +0100

    2 eme version

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Race;
+use App\Entity\LegendaryHero;
+use App\Entity\Hero;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +15,17 @@ class IndexController extends AbstractController
     /**
      * @Route("/", name="index")
      */
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
+        $races = $em->getRepository(Race::class)->findAll();
+        $legendary_heros = $em->getRepository(LegendaryHero::class)->findAll();
+        $heros = $em->getRepository(Hero::class)->findAll();
+
         return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
+            'controller_name' => 'PagesController',
+            'races' => $races,
+            'legendary_heros' => $legendary_heros,
+            'heros' => $heros,
         ]);
     }
 }
